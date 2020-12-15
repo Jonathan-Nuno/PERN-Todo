05190f9641c23a26fcbcddbc14a42b4b9a632581
Jonathan Nuno <jnuno.423@gmail.com>
2020-12-15 09:53:08 -0500

    added update todo route

--- Back/app.js
+++ Back/app.js
@@ -25,18 +25,49 @@ app.post('/todos', async (req, res) => {
     }
 })
 
-// Get todos
+// Get all Todos
 
 app.get('/todos', async (req, res) => {
     try {
         const allTodos = await pool.query(
             "SELECT * FROM todo"
-            );
-            res.json(allTodos.rows)
+        );
+        res.json(allTodos.rows)
     } catch (err) {
         console.error(err.message);
     }
 })
+
+// Get Single Todo
+app.get('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query(
+            "SELECT * FROM todo WHERE todo_id = $1",
+            [id]
+        );
+        res.json(todo.rows)
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// Update Todo
+
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description = $1 WHERE todo_id = $2",
+            [description, id]
+        );
+        res.json("Todo was updated!");
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
 
 
 app.listen(PORT, () => {
